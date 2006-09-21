5fe769edb0cfa3fd8c4c25675dfbd5917faa5d9d
taowei <taowei@998b06ac-0c1a-0410-bb68-8b0b46ddbf53>
2006-09-21 23:38:38 +0000

    optimized CC drawing time by a little

--- trunk/src/org/mindswap/swoop/utils/graph/hierarchy/HierarchicalVertexDataRenderer.java
+++ trunk/src/org/mindswap/swoop/utils/graph/hierarchy/HierarchicalVertexDataRenderer.java
@@ -78,138 +78,141 @@ public class HierarchicalVertexDataRenderer implements SizeConstants
 		
 		AffineTransform old_xform = g2d.getTransform();
 		
-		AffineTransform localForm = null;
 		double rotationAngle = 0;
 		
 		g2d.translate(x, y);
-		if (numSiblings == 0)
+		
+		
+		AffineTransform localForm = node.getLocalXForm();
+		// if nodes do not have localForm yet, compute it; otherwise, reuse it
+		if ( localForm == null )
 		{
-			g2d.fillOval( -r, -r, 2*r, 2*r );
-			g2d.setColor( node.getOutlineColor() );
-			g2d.drawOval( -r, -r, 2*r, 2*r );
-			localForm = AffineTransform.getTranslateInstance(0,0); // no op transform to prevent NullPointerException
-		}
-		else
-		{
-			double increAngle = 360.00/(numSiblings + 1);
-			double currentAngle = increAngle * index;
-			if (siblingInfo.areSameSize) // all siblings are the same size.
+			if (numSiblings == 0)
 			{
-				rotationAngle = currentAngle * 2 * Math.PI / 360.00; 
-				localForm    = AffineTransform.getRotateInstance( rotationAngle );
-				AffineTransform translation = AffineTransform.getTranslateInstance( radius/2 , 0 );
-				localForm.concatenate( translation );
-				g2d.transform( localForm );
+				localForm = AffineTransform.getTranslateInstance(0,0); // no op transform to prevent NullPointerException
 			}
-			else if ( index == 0) // largest node
+			else
 			{
-				rotationAngle = currentAngle * 2 * Math.PI / 360.00; 
-				localForm    = AffineTransform.getRotateInstance( rotationAngle );
-				AffineTransform translation = AffineTransform.getTranslateInstance( radius - r - (SizeConstants.unitSize/2), 0 );
-				localForm.concatenate( translation );
-				g2d.transform( localForm );
-			}
-			else // has at least one sibling that's larger
-			{
-				if ( siblingInfo.isFirstChildBig ) // largest sibling dominates
+				double increAngle = 360.00/(numSiblings + 1);
+				double currentAngle = increAngle * index;
+				if (siblingInfo.areSameSize) // all siblings are the same size.
 				{
-					ClassTreeNode bigSib = node.getParent().getChild(0);
-					Point2D.Double bigSibCenter = bigSib.getLocalCenter();
-					if ( index == 1)
+					rotationAngle = currentAngle * 2 * Math.PI / 360.00; 
+					localForm    = AffineTransform.getRotateInstance( rotationAngle );
+					AffineTransform translation = AffineTransform.getTranslateInstance( radius/2 , 0 );
+					localForm.concatenate( translation );
+					//g2d.transform( localForm );
+				}
+				else if ( index == 0) // largest node
+				{
+					rotationAngle = currentAngle * 2 * Math.PI / 360.00; 
+					localForm    = AffineTransform.getRotateInstance( rotationAngle );
+					AffineTransform translation = AffineTransform.getTranslateInstance( radius - r - (SizeConstants.unitSize/2), 0 );
+					localForm.concatenate( translation );
+					//g2d.transform( localForm );
+				}
+				else // has at least one sibling that's larger
+				{
+					if ( siblingInfo.isFirstChildBig ) // largest sibling dominates
 					{
-						double angle = 0;
-						double increment = 10;
-						boolean isFitting = false;
-						while ( !isFitting )
+						ClassTreeNode bigSib = node.getParent().getChild(0);
+						Point2D.Double bigSibCenter = bigSib.getLocalCenter();
+						if ( index == 1)
 						{
-							rotationAngle = (90 + 20 + angle) * 2 * Math.PI / 360.0;
+							double angle = 0;
+							double increment = 10;
+							boolean isFitting = false;
+							while ( !isFitting )
+							{
+								rotationAngle = (90 + 20 + angle) * 2 * Math.PI / 360.0;
+								localForm = AffineTransform.getTranslateInstance( bigSibCenter.x, bigSibCenter.y );
+								localForm.concatenate( AffineTransform.getRotateInstance(rotationAngle));
+								localForm.concatenate( AffineTransform.getTranslateInstance( node.getRadius() + bigSib.getRadius() + SizeConstants.unitSize, 0));
+								angle = angle + increment;
+								
+								Point2D.Double origin = new Point2D.Double(0, 0);
+								origin = (Point2D.Double)localForm.transform(origin, origin);
+								double dist = Math.sqrt( Math.pow( origin.x, 2) + Math.pow(origin.y, 2) );
+								if ( dist <= (radius - node.getRadius()))
+									isFitting = true;
+							}
+							
+							//g2d.transform( localForm );
+							
+						}
+						else
+						{		
+							/* Computing the angle needed to rotate the current node by wrt the 
+							 *  largest sibling.
+							 * Computing the distance needed to translate the current node by
+							 *  wrt to the center of the largest sibling.
+							 *  
+							 * The computed distance is the same distance between current node's
+							 *  previous sibling to the largest sibling.
+							 * The compute rotation gives a distance between the current node's
+							 *  center and its previous sibling's center of (r1 + r2 + spacing*3),
+							 *  where r1 is preivous sib's radius, r2 is current node's radius, and
+							 *  spaceing = sizeConstants.unitSize/2
+							 * 
+							 * Computing theta relies on the fact that the triangle A-B-C 
+							 *  (where A is the center of the largest sibling, B is the 
+							 *  center of the previous sibling, and C is the center of the 
+							 *  projected current node), is isoscele.  Splitting the triangle
+							 *  by bisecting its lone odd angle into 2 right triangles, we use
+							 *  arcsine to calculate the required angle.
+							 * 
+							 */
+							ClassTreeNode prevSib = node.getParent().getChild( index - 1);
+							int sibRadius  = prevSib.getRadius();
+							int thisRadius = node.getRadius();
+							int radiiSum = sibRadius + thisRadius + SizeConstants.unitSize/2 + SizeConstants.unitSize;
+							Point2D.Double sibCenter = prevSib.getLocalCenter();
+							double dCenterToSib = Math.sqrt( Math.pow(bigSibCenter.x - sibCenter.x, 2) + Math.pow(bigSibCenter.y - sibCenter.y, 2) );
+							double theta = Math.asin( (radiiSum/2) / dCenterToSib) * 2;
+									
+							rotationAngle = theta + prevSib.getRotationAngle();
+							
 							localForm = AffineTransform.getTranslateInstance( bigSibCenter.x, bigSibCenter.y );
 							localForm.concatenate( AffineTransform.getRotateInstance(rotationAngle));
-							localForm.concatenate( AffineTransform.getTranslateInstance( node.getRadius() + bigSib.getRadius() + SizeConstants.unitSize, 0));
-							angle = angle + increment;
-							
-							Point2D.Double origin = new Point2D.Double(0, 0);
-							origin = (Point2D.Double)localForm.transform(origin, origin);
-							double dist = Math.sqrt( Math.pow( origin.x, 2) + Math.pow(origin.y, 2) );
-							if ( dist <= (radius - node.getRadius()))
-								isFitting = true;
+							localForm.concatenate( AffineTransform.getTranslateInstance( dCenterToSib, 0));
+							//g2d.transform( localForm );
 						}
-						
-						g2d.transform( localForm );
-						
 					}
-					else
-					{		
-						/* Computing the angle needed to rotate the current node by wrt the 
-						 *  largest sibling.
-						 * Computing the distance needed to translate the current node by
-						 *  wrt to the center of the largest sibling.
-						 *  
-						 * The computed distance is the same distance between current node's
-						 *  previous sibling to the largest sibling.
-						 * The compute rotation gives a distance between the current node's
-						 *  center and its previous sibling's center of (r1 + r2 + spacing*3),
-						 *  where r1 is preivous sib's radius, r2 is current node's radius, and
-						 *  spaceing = sizeConstants.unitSize/2
-						 * 
-						 * Computing theta relies on the fact that the triangle A-B-C 
-						 *  (where A is the center of the largest sibling, B is the 
-						 *  center of the previous sibling, and C is the center of the 
-						 *  projected current node), is isoscele.  Splitting the triangle
-						 *  by bisecting its lone odd angle into 2 right triangles, we use
-						 *  arcsine to calculate the required angle.
-						 * 
-						 */
+					else // largest sibling isn't too dominating
+					{
 						ClassTreeNode prevSib = node.getParent().getChild( index - 1);
 						int sibRadius  = prevSib.getRadius();
 						int thisRadius = node.getRadius();
-						int radiiSum = sibRadius + thisRadius + SizeConstants.unitSize/2 + SizeConstants.unitSize;
+						int radiiSum = sibRadius + thisRadius + SizeConstants.unitSize/2;
 						Point2D.Double sibCenter = prevSib.getLocalCenter();
-						double dCenterToSib = Math.sqrt( Math.pow(bigSibCenter.x - sibCenter.x, 2) + Math.pow(bigSibCenter.y - sibCenter.y, 2) );
+						double dCenterToSib = Math.sqrt( Math.pow(sibCenter.x, 2) + Math.pow(sibCenter.y, 2) );
 						double theta = Math.asin( (radiiSum/2) / dCenterToSib) * 2;
-								
+						
 						rotationAngle = theta + prevSib.getRotationAngle();
 						
-						localForm = AffineTransform.getTranslateInstance( bigSibCenter.x, bigSibCenter.y );
-						localForm.concatenate( AffineTransform.getRotateInstance(rotationAngle));
-						localForm.concatenate( AffineTransform.getTranslateInstance( dCenterToSib, 0));
-						g2d.transform( localForm );
+						localForm    = AffineTransform.getRotateInstance( rotationAngle );
+						AffineTransform translation = AffineTransform.getTranslateInstance( dCenterToSib, 0);
+						localForm.concatenate( translation );
 					}
 				}
-				else // largest sibling isn't too dominating
-				{
-					ClassTreeNode prevSib = node.getParent().getChild( index - 1);
-					int sibRadius  = prevSib.getRadius();
-					int thisRadius = node.getRadius();
-					int radiiSum = sibRadius + thisRadius + SizeConstants.unitSize/2;
-					Point2D.Double sibCenter = prevSib.getLocalCenter();
-					double dCenterToSib = Math.sqrt( Math.pow(sibCenter.x, 2) + Math.pow(sibCenter.y, 2) );
-					double theta = Math.asin( (radiiSum/2) / dCenterToSib) * 2;
-					
-					rotationAngle = theta + prevSib.getRotationAngle();
-					
-					localForm    = AffineTransform.getRotateInstance( rotationAngle );
-					AffineTransform translation = AffineTransform.getTranslateInstance( dCenterToSib, 0);
-					localForm.concatenate( translation );
-					g2d.transform( localForm );
-				}
 			}
-			
-			g2d.fillOval( -r, -r, 2*r, 2*r );
-			g2d.setColor( node.getOutlineColor() );
-			g2d.drawOval( -r, -r, 2*r, 2*r );
+			//System.out.println("Computed localXForm");
 		}
-
-
-		double [] mat = new double [6];
-		localForm.getMatrix( mat );
 		
+		g2d.transform( localForm );
+		g2d.fillOval( -r, -r, 2*r, 2*r );
+		g2d.setColor( node.getOutlineColor() );
+		g2d.drawOval( -r, -r, 2*r, 2*r );
+
 		Point2D.Double origin = new Point2D.Double(0, 0);			
 		Point2D.Double result = (Point2D.Double)localForm.transform( origin, origin);
 		node.setLocalCenterPoint( result.x, result.y );
 		node.setGlobalTransform( g2d.getTransform() );
 		node.setRotationAngle( rotationAngle );
-		node.setAffineTransform( localForm );
+		node.setLocalXForm( localForm );
+		
+		double [] mat = new double [6];
+		localForm.getMatrix( mat );
 		
 		int numChildren = node.getNumChildren();		
 		//int maxChildRadius = getMaxChildRadius( node );
